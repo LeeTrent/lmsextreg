224cfebfb3fbcc5b0cc410bdbde64852d45176a0
LeeTrent <Lee.Trent.1@gmail.com>
2018-05-02 13:25:50 -0500

    Corrects misspelling of CountryID property

--- Models/Country.cs
+++ Models/Country.cs
@@ -2,7 +2,7 @@ namespace lmsextreg.Models
 {
     public class Country
     {
-        public int CountryyID { get; set; }
+        public int CountryID { get; set; }
         public string CountryCode{ get; set; }
         public string CountryName{ get; set; }
     }
