7dc359160d509f5d7365b40b72d0ba45d60265f6
LeeTrent <Lee.Trent.1@gmail.com>
2018-05-29 15:18:24 -0500

    Modifies ProgramEnrollment razor pages to make them presentable

--- Pages/Enrollments/Index.cshtml.cs
+++ Pages/Enrollments/Index.cshtml.cs
@@ -2,8 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+
 using Microsoft.EntityFrameworkCore;
 using lmsextreg.Data;
 using lmsextreg.Models;
@@ -13,26 +15,37 @@ namespace lmsextreg.Pages.Enrollments
     public class IndexModel : PageModel
     {
         private readonly lmsextreg.Data.ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public IndexModel(lmsextreg.Data.ApplicationDbContext context)
+        public IndexModel(lmsextreg.Data.ApplicationDbContext context, UserManager<ApplicationUser> usrMgr)
         {
             _context = context;
+            _userManager = usrMgr;
         }
 
         public IList<ProgramEnrollment> ProgramEnrollment { get;set; }
+
+        public ApplicationUser LoggedInUser {get;set;}
 
         // public async Task OnGetAsync()
         // {
         //     ProgramEnrollment = await _context.ProgramEnrollments
         //         .Include(p => p.LMSProgram).ToListAsync();
         // }
+        
         public async Task OnGetAsync()
         {
+            LoggedInUser = await GetCurrentUserAsync();
+
             ProgramEnrollment = await _context.ProgramEnrollments
                 .Include(p => p.LMSProgram)
                 .Include(p => p.EnrollmentStatus)
                 .Include(p => p.Learner)
                 .ToListAsync();
-        }        
+        }
+
+        private Task<ApplicationUser> GetCurrentUserAsync() => _userManager.GetUserAsync(HttpContext.User);
+
+
     }
 }
