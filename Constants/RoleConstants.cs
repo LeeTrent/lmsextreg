eeb9d7b7e2031d33eb4653b3b016118ce73d698f
LeeTrent <Lee.Trent.1@gmail.com>
2018-06-04 16:19:19 -0500

    Changes name of role from 'Learner' to 'Student'

--- Constants/RoleConstants.cs
+++ Constants/RoleConstants.cs
@@ -2,7 +2,7 @@ namespace lmsextreg.Constants
 {
     public class RoleConstants
     {
-        public static readonly string LEARNER = "LEARNER";
+        public static readonly string STUDENT = "STUDENT";
         public static readonly string APPROVER = "APPROVER";
         public static readonly string ADMIN = "ADMIN";
     }
