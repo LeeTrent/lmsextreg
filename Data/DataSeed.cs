92c73e5ff169d946e5b48545245ed81d55bf55e1
LeeTrent <Lee.Trent.1@gmail.com>
2018-06-06 12:32:46 -0500

    Now seeding data, specifically role data

--- Data/DataSeed.cs
+++ Data/DataSeed.cs
@@ -8,15 +8,16 @@ namespace lmsextreg.Data
 {
     public static class DataSeed
     {
-        public static async Task Initialize (IServiceProvider svcProvider)
+        public static async Task Initialize (IServiceProvider svcProvider, string tempPW)
         {
             await EnsureRoles(svcProvider);
+            //await EnsureApprovers(svcProvider, tempPW); 
         }
 
         private static async Task EnsureRoles(IServiceProvider svcProvider)
         {
             await EnsureRole(svcProvider, RoleConstants.STUDENT);
-            await EnsureRole(svcProvider, RoleConstants.APPROVER);    
+            await EnsureRole(svcProvider, RoleConstants.APPROVER);   
         }
         
         private static async Task<IdentityResult> EnsureRole(IServiceProvider svcProvider, string roleName)
@@ -32,6 +33,40 @@ namespace lmsextreg.Data
                  
             }
             return IR;
+        }
+
+        private static async Task EnsureApprovers(IServiceProvider svcProvider, string tempPW)
+        {
+            var userID = await EnsureApprover(svcProvider, "lee.trent@icloud.gov", tempPW);
+            await EnsureUserRole(svcProvider, RoleConstants.APPROVER, userID);
+
+            userID = await EnsureApprover(svcProvider, "lee.trent.1@outlook.gov", tempPW);
+            await EnsureUserRole(svcProvider, RoleConstants.APPROVER, userID);            
+        }
+        private static async Task<string> EnsureApprover(IServiceProvider svcProvider, string userName, string tempPW)
+        {
+            var userMgr = svcProvider.GetService<UserManager<ApplicationUser>>();
+            
+            var user = await userMgr.FindByNameAsync(userName);
+            if (user == null)
+            {
+                user = new ApplicationUser
+                {
+                    UserName = userName
+                };
+            
+                await userMgr.CreateAsync(user, tempPW);
+            }
+            
+            return user.Id;
+        }
+
+        private static async Task<IdentityResult> EnsureUserRole(IServiceProvider svcProvider, string roleName, string userID)
+        {
+            var userMgr = svcProvider.GetService<UserManager<ApplicationUser>>();
+            var user = await userMgr.FindByIdAsync(userID);
+
+            return await userMgr.AddToRoleAsync(user, roleName);
         }
      }
 }
