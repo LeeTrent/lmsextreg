bf6ce544424190a4459fe91e42dd19193182d8ec
LeeTrent <Lee.Trent.1@gmail.com>
2018-06-14 10:26:31 -0500

    Completes StatusTransition model class

--- Models/StatusTransition.cs
+++ Models/StatusTransition.cs
@@ -5,12 +5,18 @@ namespace lmsextreg.Models
     public class StatusTransition
     {
         [Required]
-        public string FromStatusCode { get; set; }
+        public int StatusTransitionID { get; set; }
         [Required]
+        public string FromStatusCode { get; set; }
         public string ToStatusCode { get; set; }
         [Required]
         public string ActionCode { get; set; }
         [Required]        
         public string ActionLabel { get; set; }
+    
+        // Navigation Properties
+       public EnrollmentStatus FromStatus {get; set; }
+        [Required]
+        public EnrollmentStatus ToStatus { get; set; }        
     }
 }
