603c9ce294c71793e45476502685b0c725bfa2b8
LeeTrent <Lee.Trent.1@gmail.com>
2018-07-22 15:01:17 -0500

    Adds new Status Code Transitions

--- Constants/TransitionCodeConstants.cs
+++ Constants/TransitionCodeConstants.cs
@@ -6,7 +6,10 @@ namespace lmsextreg.Constants
         public static readonly string PENDING_TO_WITHDRAWN  = "PENDING_TO_WITHDRAWN";
         public static readonly string PENDING_TO_APPROVED   = "PENDING_TO_APPROVED";
         public static readonly string PENDING_TO_DENIED     = "PENDING_TO_DENIED";   
+        public static readonly string APPROVED_TO_WITHDRAWN = "APPROVED_TO_WITHDRAWN";
         public static readonly string APPROVED_TO_REVOKED   = "APPROVED_TO_REVOKED";
+        public static readonly string WITHDRAWN_TO_PENDING  = "WITHDRAWN_TO_PENDING";
         public static readonly string DENIED_TO_APPROVED    = "DENIED_TO_APPROVED";
+        public static readonly string REVOKED_TO_PENDING    = "REVOKED_TO_PENDING";
     }
 }
